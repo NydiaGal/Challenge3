7b9820c466822a13b8193f5c93525c5b40aef05a
NydiaGal <135789457+NydiaGal@users.noreply.github.com>
2023-09-08 20:54:48 -0500

    vars for prompts and validation

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,46 @@
-// Assignment code here
-
-
-// Get references to the #generate element
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+var passwordLength = prompt("Enter desired password length");
+console.log (generatePassword);
+
+function generatePassword ()
+
+// Prompt the user to choose a length of at least 8 characters and no more than 128 characters
+// Store in variable (name it)
+// If it isnt, keep prompting until they enter a valid value.  
+// maybe use a while loop for this.
+// for loop doesnt really work because loops are usually use for a set of number iterations.
+// Remember: prompt always returns a string even if the string is made up of numbers.
+// Convert the string entered by user to a number. This may be an issue to address.
+if ("42" > "8") {
+  console.log("It's a greater than minimum");
+}
+// Ask user if they want to include the following:
+// Prompt the user to confirm if using uppercase.
+// Prompt the user to confirm if using lowercase.
+// Prompt the user to confirm if using numbers.
+// Prompt the user to confirm if using special characters (https://www.owasp.org/index.php/Password_special_characters)
+// Track answers
+// Probably use # character until #
+// Use the confirm() method to get the Boolean value/answer to prompts
+// Validate input that at least one character type should be selected
+// WHEN all prompts are answered.
+// Generate a random password based on criteria provided
+// If no criteria is provided - Display alert "Unable to generate password due to minimum criteria not being met"
+//  
+
+
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWYXZ";
+var numbers = "0123456789";
+var special = "! #$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
 
   passwordText.value = password;
 
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Password Generator Starter Code
+# Challenge 3 - Generate a random password based on user criteria
+
+## Instructions
+
+The completed application should meet the following criteria:
+
+// Prompt user to click a button to generate a random password criteria
+// Prompt the user to select criteria to include in the password
+// Prompt the user to choose a length of at least 8 characters and no more than 128 characters
+// Store in variable (name it)
+// Prompt the user to confirm if using uppercase.
+// Prompt the user to confirm if using lowercase.
+// Prompt the user to confirm if using numbers.
+// Prompt the user to confirm if using special characters (https://www.owasp.org/index.php/Password_special_characters)
+// Track answers
+// Validate input that at least one character type should be selected
+// WHEN all prompts are answered, math.floor, math.random (random decimal number between 0-1) (characted codes)
+// add that character to a string (concatenate to end of ongoing pw being build)
+// Return new password/Generate a random password based on criteria provided
+// If no criteria is provided - Display alert "Unable to generate password due to minimum criteria not being met"
